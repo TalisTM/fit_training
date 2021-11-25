3ef6e9bb612956a5f8a471770863a3322034df7e
talis <talismarchioro@gmail.com>
2021-11-24 21:44:55 -0300

    fist commit

---
+++ android/app/src/main/kotlin/com/talis/fit_training/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.talis.fit_training
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:fit_training/presentation/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Fit Training",
+      home: HomePage(),
+    );
+  }
+}
