64dc115b4dec21838b2a0d3f30265537b10ee324
talis <talismarchioro@gmail.com>
2022-01-25 13:52:11 -0300

    card do inicio no meio e ajustrado linha preta na tela

--- lib/presentation/pages/home/widgets/app_bar_home.dart
+++ lib/presentation/pages/home/widgets/app_bar_home.dart
@@ -21,98 +21,98 @@ class AppBarHome extends StatelessWidget implements PreferredSizeWidget {
 
     return PreferredSize(
       preferredSize: const Size.fromHeight(300),
-      child: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(left: 15),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                image: NetworkImage(userStore.user.photoUrl!)
+      child: Container(
+        color: Theme.of(context).primaryColor,
+        child: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(left: 15),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  image: NetworkImage(userStore.user.photoUrl!)
+                                )
+                              ),
+                              height: 50,
+                              width: 50,
+                            ),
+                            const SizedBox(width: 15),
+                            Text.rich(
+                              TextSpan(
+                                text: "Olá, ",
+                                style: Theme.of(context).textTheme.headline4,
+                                children: [
+                                  TextSpan(
+                                    text: userStore.user.name,
+                                    style: Theme.of(context).textTheme.headline3
+                                  )
+                                ]
                               )
                             ),
-                            height: 50,
-                            width: 50,
-                          ),
-                          const SizedBox(width: 15),
-                          Text.rich(
-                            TextSpan(
-                              text: "Olá, ",
-                              style: Theme.of(context).textTheme.headline4,
-                              children: [
-                                TextSpan(
-                                  text: userStore.user.name,
-                                  style: Theme.of(context).textTheme.headline3
-                                )
-                              ]
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.logout_outlined, color: Theme.of(context).splashColor, size: 30,),
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => DialogWidget(
+                                    title: "Atenção",
+                                    subTitle: "Tem certeza que deseja deslogar do aplicativo?",
+                                    primarylabel: "Confirmar",
+                                    secundaryLabel: "Cancelar",
+                                    primaryFunc: () async {
+                                      await FirebaseAuth.instance.signOut();
+                                      Navigator.push(context, MaterialPageRoute(builder: (context) => const AuthPage()));
+                                    }, 
+                                    secundaryFunc: () => Navigator.pop(context)
+                                  )
+                                );
+                              },
                             )
-                          ),
-                        ],
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15)
+                  ],
+                )
+              ),
+              const SizedBox(height: 5),
+              Stack(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        color: Theme.of(context).primaryColor,
+                        height: 40, 
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.logout_outlined, color: Theme.of(context).splashColor, size: 30,),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (context) => DialogWidget(
-                                  title: "Atenção",
-                                  subTitle: "Tem certeza que deseja deslogar do aplicativo?",
-                                  primarylabel: "Confirmar",
-                                  secundaryLabel: "Cancelar",
-                                  primaryFunc: () async {
-                                    await FirebaseAuth.instance.signOut();
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => const AuthPage()));
-                                  }, 
-                                  secundaryFunc: () => Navigator.pop(context)
-                                )
-                              );
-                            },
-                          )
-                        ],
+                      Container(
+                        color: Theme.of(context).backgroundColor,
+                        height: 50,
                       ),
                     ],
                   ),
-                  const SizedBox(height: 15)
+                  const AbstractData()
                 ],
               )
-            ),
-            Container(
-              color: Theme.of(context).primaryColor,
-              height: 10
-            ),
-            Stack(
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      color: Theme.of(context).primaryColor,
-                      height: 50, 
-                    ),
-                    Container(
-                      color: Theme.of(context).backgroundColor,
-                      height: 50,
-                    ),
-                  ],
-                ),
-                const AbstractData()
-              ],
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
