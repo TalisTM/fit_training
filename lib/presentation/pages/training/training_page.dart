cdde3145c85fef53e4b459a453432a368f08e134
talis <talismarchioro@gmail.com>
2022-02-24 10:49:06 -0300

    verificando se esta marcado o check quando finalizar o treino | zerando o done e false no check quando finaliza

--- lib/presentation/pages/training/training_page.dart
+++ lib/presentation/pages/training/training_page.dart
@@ -1,3 +1,4 @@
+import 'package:fit_training/database/database.dart';
 import 'package:fit_training/models/training_entity.dart';
 import 'package:fit_training/presentation/components/widgets/appbar_widget.dart';
 import 'package:fit_training/presentation/components/widgets/button_widget.dart';
@@ -35,25 +36,55 @@ class _TrainingPageState extends State<TrainingPage> {
         child: ButtonWidget(
           label: "Finalizar",
           onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) => DialogWidget(
-                title: "Finalizar treino",
-                subTitle: "Tem certeza que deseja finalizar este treino?",
-                primarylabel: "Confirmar",
-                secundaryLabel: "Cancelar",
-                primaryFunc: () {
-                  userStore.addDone();
-                  //atualizar nanmcooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-                  Navigator.pop(context);
-                  Navigator.pop(context);
-                },
-                secundaryFunc: () => Navigator.pop(context)
-              )
-            );
+            bool notCheck = false;
+            for (var exercise in widget.training.exercises!) {
+              if(!exercise.check!) {
+                notCheck = true;
+              }
+            }
+            
+            if(notCheck) {
+              showDialog(
+                context: context,
+                builder: (context) => DialogWidget(
+                  title: "Tem certeza?",
+                  subTitle: "Ainda há exercícios para concluir.",
+                  primarylabel: "Sim",
+                  secundaryLabel: "Cancelar",
+                  primaryFunc: finalizarTreino,
+                  secundaryFunc: () => Navigator.pop(context)
+                )
+              );
+            } else {
+              showDialog(
+                context: context,
+                builder: (context) => DialogWidget(
+                  title: "Finalizar treino",
+                  subTitle: "Tem certeza que deseja finalizar este treino?",
+                  primarylabel: "Sim",
+                  secundaryLabel: "Cancelar",
+                  primaryFunc: finalizarTreino,
+                  secundaryFunc: () => Navigator.pop(context)
+                )
+              );
+            }
           }
         ),
       )
     );
+  }
+
+  void finalizarTreino() {
+    userStore.addDone();
+     
+    for (var exercise in widget.training.exercises!) {
+      exercise.done = 0;
+      exercise.check = false;
+    }
+
+    Database.saveTraining();
+
+    Navigator.pop(context);
+    Navigator.pop(context);
   }
 }
